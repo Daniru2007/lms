57fe0c1a2f6721a7c8a67711fbf3f862a9a64cf4
Daniru2007 <daniruayuka@gmail.com>
2022-11-24 22:32:30 +0530

    styling

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Learning Management System (LMS)
+# Learning Management System (LMS)
--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,27 @@
 <script>
+    import { Router, Route, Link } from "svelte-routing";
+    export let url = "";
+    let dark = true;
+    $: theme = dark ? "dark" : "light";
 </script>
 
-<main>Hello World!</main>
+<Router {url}>
+    <nav class="nav-bar {theme}">
+        <div class="div0" />
+        <div class="div1">
+            <Link to="/">Home</Link>
+            <Link to="/">Courses</Link>
+            <Link to="/">Colleges</Link>
+        </div>
+
+        <div class="div2">
+            <Link to="/">Sign in</Link>
+            <Link to="/">Join</Link>
+        </div>
+    </nav>
+</Router>
+
+<button on:click={() => (dark = !dark)}> switch themes </button>
 
 <style>
 </style>
