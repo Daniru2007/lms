dddd37ba9f8c52415d11d61681d20ae57b0adce0
Daniru2007 <daniruayuka@gmail.com>
2022-11-25 19:27:34 +0530

    navbar

--- src/App.svelte
+++ src/App.svelte
@@ -3,25 +3,37 @@
     export let url = "";
     let dark = true;
     $: theme = dark ? "dark" : "light";
+    $: icon = dark ? "🌛" : "🌞";
 </script>
 
-<Router {url}>
-    <nav class="nav-bar {theme}">
-        <div class="div0" />
-        <div class="div1">
-            <Link to="/">Home</Link>
-            <Link to="/">Courses</Link>
-            <Link to="/">Colleges</Link>
-        </div>
+<body class={theme}>
+    <Router {url}>
+        <nav class="nav-bar {theme}">
+            <div class="div0" />
+            <div class="div1">
+                <Link to="/">Home</Link>
+                <Link to="/">Courses</Link>
+                <Link to="/">Colleges</Link>
+            </div>
 
-        <div class="div2">
-            <Link to="/">Sign in</Link>
-            <Link to="/">Join</Link>
-        </div>
-    </nav>
-</Router>
+            <div>
+                <button
+                    class="change-theme {theme}"
+                    on:click={() => (dark = !dark)}>{icon}</button
+                >
+            </div>
 
-<button on:click={() => (dark = !dark)}> switch themes </button>
+            <div class="div2">
+                <Link to="/">Sign in</Link>
+                <Link to="/">Join</Link>
+            </div>
+        </nav>
+    </Router>
+
+    <section class="home {theme}">
+        <div class={theme}><h1>Hello</h1></div>
+    </section>
+</body>
 
 <style>
 </style>
